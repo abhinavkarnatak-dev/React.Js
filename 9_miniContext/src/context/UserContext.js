57b218f4575a86359119fa2b9d9d43c10db1f498
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2024-07-24 23:14:06 +0530

    Learned about context api in react

---
+++ 9_miniContext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserContext = React.createContext() // Context is like a global variable
+
+export default UserContext
+
+// We use a context provider to provide data
